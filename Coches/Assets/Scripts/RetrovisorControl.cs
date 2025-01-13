b7077bd4778b742f6958c9e434a2c3238898f1a5
davidsaloz <dsalloz231@g.educaand.es>
2025-01-13 19:10:59 +0100

    tutorial

---
+++ Coches/Assets/Scripts/RetrovisorControl.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class RetrovisorControl : MonoBehaviour
+{
+    [SerializeField] GameObject retrovisor;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R)){
+            retrovisor.SetActive(true);
+        }
+
+        if(Input.GetKeyUp(KeyCode.R)){
+            retrovisor.SetActive(false);
+        }
+    }
+}
